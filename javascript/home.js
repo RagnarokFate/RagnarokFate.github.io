a2040b903cd3ba0f9c40dd9411022a71cf0583cb
PhoenixOfDoom <B.1.11.11@hotmail.com>
2024-03-04 18:08:12 +0200

    added analytics to index.html page only (removed from home.html)

--- javascript/home.js
+++ javascript/home.js
@@ -14,9 +14,3 @@ document.addEventListener("DOMContentLoaded", function () {
     fadeInSequence(0);
 });
 
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-T35P8M0JGD');
